804a7a8b03244c39c36bb8fcc16e4f6852391a74
luongnd2625 <luongnd2625@gmail.com>
2025-11-04 22:53:53 +0700

    Create query and sql code
    I create all necessary query and sql for controller's function (not include filter, I will add later)

--- asm_FA25/src/java/dal/RequestDAO.java
+++ asm_FA25/src/java/dal/RequestDAO.java
@@ -1,4 +1,139 @@
 package dal;
-public class RequestDAO {
+
+import Model.Request;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.sql.Types;
+
+public class RequestDAO extends DBcontext{
+    //Get All request
+    public List<Request> getAll() {
+        List<Request> list = new ArrayList<>();
+        String sql = "SELECT * FROM Request";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                int reqID = rs.getInt("reqID");
+                String title = rs.getNString("title");
+                int userID = rs.getInt("userID");
+                Date fromDate = rs.getDate("fromDate");
+                Date toDate = rs.getDate("toDate");
+                String reason = rs.getString("reason");
+                int statusID = rs.getInt("statusID");
+                Integer approverID = rs.getInt("approverID");
+                String approverNote = rs.getString("approverNote");
+                Request r = new Request(reqID, title, userID, fromDate, toDate, reason, statusID, approverID, approverNote);
+                list.add(r);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return list;
+    }
+    
+    //Get request by userID (To show requests from account that logged in app)
+    public List<Request> getRequestByUserID(int userID) {
+        List<Request> list = new ArrayList<>();
+        String sql = "SELECT * FROM Request WHERE userID = ?";
+        
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, userID);
+            ResultSet rs = st.executeQuery();
+            while(rs.next()) {
+                int reqID = rs.getInt("reqID");
+                String title = rs.getString("title");
+                Date fromDate = rs.getDate("fromDate");
+                Date toDate = rs.getDate("toDate");
+                String reason = rs.getString("reason");
+                int statusID = rs.getInt("statusID");
+                Integer approverID = rs.getInt("approverID");
+                String approverNote = rs.getString("approverNote");
+                Request r = new Request(reqID, title, userID, fromDate, toDate, reason, statusID, approverID, approverNote);
+                list.add(r);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return list;
+    }
+    //Add request to db and list
+    public void addRequest(Request request) {
+        String query = "INSERT INTO Request(title, userID, fromDate, toDate, reason, statusID, approverID, approverNote) VALUES (?, ?, ?, ?, ?, ?, ?, ?) ";
+        try {
+            PreparedStatement st = connection.prepareStatement(query);
+            st.setString(1, request.getTitle());
+            st.setInt(2, request.getUserID());
+            st.setDate(3, (java.sql.Date) request.getFromDate());
+            st.setDate(4, (java.sql.Date) request.getToDate());
+            st.setString(5, request.getReason());
+            st.setInt(6, request.getStatusID());
+
+            if (request.getApproverID() != null) {
+                st.setInt(7, request.getApproverID());
+
+                // Nếu có người duyệt, có thể có ghi chú
+                if (request.getApproverNote() != null && !request.getApproverNote().trim().isEmpty()) {
+                    st.setString(8, request.getApproverNote());
+                } else {
+                    st.setNull(8, java.sql.Types.NVARCHAR);
+                }
+            } else {
+                // Nếu chưa có người duyệt, cả hai trường phải NULL
+                st.setNull(7, java.sql.Types.INTEGER);
+                st.setNull(8, java.sql.Types.NVARCHAR);
+            }
+            st.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    //Update request(Change request to <Approved> or <Reject> if approver want) 
+    public void updateRequest(Request request) {
+        String query = "UPDATE Request SET title = ?, fromDate = ?, toDate = ?, reason = ?, approverID = ?, approverNote = ?";
+        try {
+            PreparedStatement st = connection.prepareStatement(query);
+            st.setString(1, request.getTitle());
+            st.setDate(2, (java.sql.Date) request.getFromDate());
+            st.setDate(3, (java.sql.Date) request.getToDate());
+            st.setString(4, request.getReason());
+            st.setInt(5, request.getApproverID());
+            st.setString(6, request.getApproverNote());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //Approve Or Reject Request
+    public void approveRequest(Request request) {
+        String query = "UPDATE Request SET statusID = ?, approverID = ?, approverNote = ? WHERE approverID = ?";
+        try {
+            PreparedStatement st = connection.prepareStatement(query);
+            st.setInt(1, request.getStatusID());
+            st.setInt(2, request.getApproverID());
+            st.setString(3, request.getApproverNote());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //Delete(Cancel) request
+    public void deleteRequest(int reqID) {
+        String query = "UPDATE Request SET statusID = 4 WHERE reqID = ?";
+        try {
+            PreparedStatement st = connection.prepareStatement(query);
+            st.setInt(1, reqID);
+            st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    //Filter of request
     
 }
--- asm_FA25/src/java/dal/UsersDAO.java
+++ asm_FA25/src/java/dal/UsersDAO.java
@@ -1,13 +1,175 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dal;
 
-/**
- *
- * @author Ber
- */
-public class UsersDAO {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import Model.Users;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UsersDAO extends DBcontext{
+    //Login
+    public Users getUsersByUsernameAndPassword(String username, String password) {
+        Users account = null;
+        String query = "SELECT * FROM Users WHERE username = ? AND password = ?";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(query);
+            st.setString(1, username);
+            st.setString(2, password);
+            ResultSet rs = st.executeQuery();
+            if(rs.next()){
+                int userID = rs.getInt("userID");
+                String fullname = rs.getString("fullname");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                int deptID = rs.getInt("deptID");
+                int roleID = rs.getInt("roleID");
+                account = new Users(userID, username, password, fullname, email, phone, deptID, roleID);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return account;
+    }
+    
+    //Get all users
+    public List<Users> getAll() {
+        List<Users> list = new ArrayList<>();
+        String sql = "SELECT * FROM Users";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while(rs.next()){
+                int userID = rs.getInt("userID");
+                String username = rs.getString("Username");
+                String password = rs.getString("password");
+                String fullname = rs.getString("fullname");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                int deptID = rs.getInt("deptID");
+                int roleID = rs.getInt("roleID");
+                Users users = new Users(userID, username, password, fullname, email, phone, deptID, roleID);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return list;
+    }
+    
+    //Get all user expect admin
+    public List<Users> getAllNotAdmin() {
+        List<Users> list = new ArrayList<>();
+        String sql = "SELECT * FROM Users where userID != 0";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            while(rs.next()){
+                int userID = rs.getInt("userID");
+                String username = rs.getString("Username");
+                String password = rs.getString("password");
+                String fullname = rs.getString("fullname");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                int deptID = rs.getInt("deptID");
+                int roleID = rs.getInt("roleID");
+                Users users = new Users(userID, username, password, fullname, email, phone, deptID, roleID);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return list;
+    }
+    //Filter for roleID
+    public List<Users> filterByRoleID(int roleID) {
+        List<Users> list = new ArrayList<>();
+        String sql = "SELECT * FROM Users WHERE roleID = ?";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, roleID);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                int userID = rs.getInt("userID");
+                String username = rs.getString("Username");
+                String password = rs.getString("password");
+                String fullname = rs.getString("fullname");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                int deptID = rs.getInt("deptID");
+                Users user = new Users(userID, username, password, fullname, email, phone, deptID, roleID);
+                list.add(user);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return list;
+    }
+
+    //Add user
+    public void addUser(Users users){
+        String query = "INSERT INTO Users (username, password, fullname, email, phone, deptID, roleID) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement st = connection.prepareStatement(query);
+            st.setString(1, users.getUsername());
+            st.setString(2, users.getPassword());
+            st.setString(3, users.getFullname());
+            st.setString(4, users.getEmail());
+            st.setString(5, users.getPhone());
+            st.setInt(6, users.getDeptID());
+            st.setInt(7, users.getRoleID());
+            st.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    //Update user's information
+     public void editUser(Users users){
+        String query = "UPDATE Users SET username = ?, password = ?, fullname = ?, email = ?, phone = ?, deptID = ?, roleID = ? WHERE userID =?";
+        try {
+            PreparedStatement st = connection.prepareStatement(query);
+            st.setString(1, users.getUsername());
+            st.setString(2, users.getPassword());
+            st.setString(3, users.getFullname());
+            st.setString(4, users.getEmail());
+            st.setString(5, users.getPhone());
+            st.setInt(6, users.getDeptID());
+            st.setInt(7, users.getRoleID());
+            st.setInt(8, users.getUserID());
+            st.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+    //Delete user in db
+    public void deleteUsers(int userID) {
+        String sql = "DELETE from Users WHERE userID = ?";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, userID);
+            st.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+    
+    //Check username
+    public boolean isUsernameTaken(String username) {
+        String query = "SELECT * FROM Users WHERE username = ?";
+        try {
+            PreparedStatement st = connection.prepareStatement(query);
+            st.setString(1, username);
+            ResultSet rs = st.executeQuery();
+            return rs.next(); //If found username, true
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+    
     
 }
