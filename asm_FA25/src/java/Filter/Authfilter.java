0d47150254c5242ede762d6eae00eb021f2b2315
luongnd2625 <luongnd2625@gmail.com>
2025-11-05 20:24:52 +0700

    Config Filter
    Config filter class to navigate by roleID of user that logged in at loginPage.

--- asm_FA25/src/java/Filter/Authfilter.java
+++ asm_FA25/src/java/Filter/Authfilter.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Filter;
+import Model.Users;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import java.io.IOException;
 
-/**
- *
- * @author Ber
- */
-public class Authfilter {
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter(urlPatterns = "/adminUserManagement")
+public class Authfilter implements Filter{
+    
+    private static final boolean debug = true;
+    private FilterConfig filterConfig = null;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession(false);
+        
+        boolean loggedIn = (session != null && session.getAttribute("user") != null);
+        
+        String requestURI = httpRequest.getRequestURI();
+        
+        boolean LoginPage = requestURI.endsWith("/login");
+        boolean RegisterPage = requestURI.endsWith("/register");
+        boolean LogoutPage = requestURI.endsWith("/logout");
+        
+        boolean UserAgendaPage = requestURI.endsWith("/userAgenda");
+        boolean UserRequestManagement = requestURI.endsWith("/userRequestManagement");
+        boolean UserRequest = requestURI.endsWith("/userRequest");
+        boolean UserRequestView = requestURI.endsWith("/userRequestView");
+        boolean UserSettings = requestURI.endsWith("/userSettings");
+        
+        
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
     
 }
