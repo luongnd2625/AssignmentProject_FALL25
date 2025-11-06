f89332073c8ddbabd9e7277e306c41f6e9f2d802
luongnd2625 <luongnd2625@gmail.com>
2025-11-06 15:04:12 +0700

    Update Logout
    Update Logout Servlet to logout user

--- asm_FA25/src/java/Controller/LogoutServlet.java
+++ asm_FA25/src/java/Controller/LogoutServlet.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controller;
 
-/**
- *
- * @author Ber
- */
-public class LogoutServlet {
-    
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //Get session of user
+        HttpSession session = request.getSession(false); // false = không tạo session mới nếu chưa có
+        
+        if (session != null) {
+            // Remove session of user
+            session.invalidate();
+        }
+        
+        // Forward user to Login page
+        response.sendRedirect("login");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //Forward to doGet
+        doGet(request, response);
+    }
 }
